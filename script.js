102fe2a8040c9f04101b045ba65e51752e3c9edd
Zuruu12 <rayhannavis11@gmail.com>
2024-03-25 09:22:26 +0700

    initial update

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function button1() {
   document.getElementById("main").style.backgroundColor = "#E12E2E";
   document.getElementById("descjob").innerHTML = "Anda adalah seorang Teknis!";
   document.getElementById("header").innerHTML = "Anda Berbohong!";
-  document.getElementById("imgjob").src = "/asset/2.png";
+  document.getElementById("imgjob").src =
+    "https://s3-alpha-sig.figma.com/img/b8fd/d7c0/bcb2a8115cc9610a0c1a9d78ccb9a051?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Sm7U4Sb2Y5lsDvy~QCdwNiisqJqaKYhREeKfDmR6IOFqFoAHvfYRvt6x1MGj3guOYiJeFgMH6UvQ-dMiAF5k-gyJsMRNImD5kusMI3nbhQv4abyYvY4ozUstxrd4l4wbj2qO~iuBxBhEG3uELrd5HbWsppKL9DSW2CUvZaFT1~XrKaA7zIJewV8Gpq3yi3um9K9yJeq7AnCZWXOkim3mQOQp1cXdcqheoeUUIDalt5o9LuktIR~t5JgCxQ6LKEQDI8d7U8efx9rPi2SCPeibee7jZp~vTHOekVyhLLm-xqNScI8V9MjiwoMs3SzV0EQkgyx5pv6uKAsXL39eJF4I0Q__";
   const element = document.getElementById("button");
   element.remove();
 }
@@ -11,7 +12,8 @@ function button2() {
   document.getElementById("main").style.backgroundColor = "#6FD240";
   document.getElementById("descjob").innerHTML = "Anda adalah seorang Mahasiswa";
   document.getElementById("header").innerHTML = "Anda Benar!";
-  document.getElementById("imgjob").src = "/asset/3.png";
+  document.getElementById("imgjob").src =
+    "https://s3-alpha-sig.figma.com/img/f0a4/f46d/22b0da421086d344420bcc7fb45598b1?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=i7plS-dAuX5lkME1gklmfydl2tGgZ85eiCcC6LlXV9Vf4M6IkAqA7ZxYddxesJX9i~TIh8I2bBccfMuwGENv3BIc0hC0bJKcvsG2eDxXUyEu9O~WGZ-ocLIRVcdxDlxkXFE-K2ENbI2EfKby6HqYY4x7fxY9sTMil87LWBWleHzzpuDKWPWf40q6ev-cwtsnU0Q63LUQDPU9gFFwY-4DKmXSyfwfU3CHxyWL7vc7Fj~AQ1eOtzJDaN6C24qwnfRdeZYVAJNozobfZ41r-uI4vdrt5SFUjN1SazYyB-3qHIGsrgkI5300wiHaMbfWlLK6~OSnAv5iaiZhfxaH4iDL5w__";
   const element = document.getElementById("button");
   element.remove();
 }
